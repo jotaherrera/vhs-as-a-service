b861e4946495f54cbd7720e11b28ee2f78fe733c
Juan Herrera <juanherreramen13@gmail.com>
2025-12-14 18:45:03 -0500

    feat: #2 implement role operations and refine role model
    - Introduce role operations for retrieving and creating roles
      in the database
    - Update the role model to ensure the `onupdate` function
      is correctly called and remove the default value for `is_active`
      to enforce explicit role activation status

---
+++ app/operations/role.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import Session
+
+from app.models import Role
+from app.schemas.role import RoleCreate, RoleResponse
+
+
+def get_role_by_name(db: Session, name: str) -> Role | None:
+    return db.query(Role).filter(Role.name == name).first()
+
+
+def get_role_by_id(db: Session, role_id: int) -> Role | None:
+    return db.query(Role).filter(Role.id == role_id).first()
+
+
+def create_role(db: Session, role: RoleCreate) -> RoleResponse:
+    db_role = Role(
+        name=role.name,
+    )
+    db.add(db_role)
+    db.commit()
+    db.refresh(db_role)
+    return db_role
