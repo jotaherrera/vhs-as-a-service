0b36d700f8ab0e7408184d7473e7b4c9dca3526f
Juan Herrera <juanherreramen13@gmail.com>
2026-01-04 17:51:08 -0500

    chore: #2 make project portable
    - Remove patches
    - Add a sql database setup
    - Update gitignore
    - Remove scripts since with a portable
      approach they are no longer necessary

--- app/database/sql/2025_12_14_1910_move_tables.sql
+++
@@ -1,3 +0,0 @@
--- Move tables from public to app
-ALTER TABLE IF EXISTS public.roles SET SCHEMA app;
-ALTER TABLE IF EXISTS public.users SET SCHEMA app;
--- app/database/sql/2025_12_14_2108_move_alembic_table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE IF EXISTS public.alembic_version SET SCHEMA app;
