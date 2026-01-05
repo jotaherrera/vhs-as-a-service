eb97162a73611381b6c24adb4a9c5a00c1ca4443
Juan Herrera <juanherreramen13@gmail.com>
2026-01-04 20:52:38 -0500

    feat: #2 add database seeding functionality
    - Introduce seeders for roles and super user creation
    - Create bootstrap script to execute seeders

---
+++ app/database/seeders/create_roles.py
@@ -0,0 +1,24 @@
+import logging
+
+from app.database.session import get_db
+from app.operations import role as crud_role
+from app.schemas.role import RoleCreate
+
+LOGGER = logging.getLogger(__name__)
+
+
+def create_roles() -> None:
+    roles = [
+        RoleCreate(name="admin"),
+        RoleCreate(name="user"),
+    ]
+
+    with get_db() as db:
+        for role in roles:
+            existing_role = crud_role.get_role_by_name(db, role.name)
+            if existing_role:
+                msg = f"Role {role.name} already exists"
+                LOGGER.warning(msg)
+                continue
+
+            crud_role.create_role(db, role)
