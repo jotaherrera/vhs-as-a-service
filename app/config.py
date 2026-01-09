87a736f2679c8ef9146c5dda8ee09fcb418b4ecd
Juan Herrera <juanherreramen13@gmail.com>
2026-01-08 22:17:57 -0500

    fix: #2 update settings usage
    - Use function get_settings properly

--- app/config.py
+++ app/config.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy.engine.url import URL
@@ -39,4 +41,6 @@ class Settings(BaseSettings):
     database: DatabaseSettings
 
 
-settings = Settings()
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
--- app/database/session.py
+++ app/database/session.py
@@ -5,7 +5,9 @@ from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from app.config import settings
+from app.main import get_settings
+
+settings = get_settings()
 
 engine = create_engine(
     settings.database.connection_url,
