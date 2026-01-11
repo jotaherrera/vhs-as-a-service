223621d941bc67921ec4f4a196092498ce32c0df
Juan Herrera <juanherreramen13@gmail.com>
2026-01-10 22:17:30 -0500

    feat: #2 add user authorization
    - Add user auth to get_all_users endpoint

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,13 @@
+from typing import Annotated
+
 from fastapi import APIRouter
+from fastapi.params import Depends
 from pydantic import BaseModel
 
 from app.database.session import DbSession
+from app.dependencies.auth import get_current_active_user
+from app.exceptions import ForbidenError
+from app.models.user import User
 from app.operations import user as crud_user
 from app.schemas.user import UserResponse
 
@@ -14,9 +20,13 @@ class AllUsersResponse(BaseModel):
 
 
 @router.get("/users")
-async def get_all_users(db: DbSession) -> AllUsersResponse:
+async def get_all_users(
+    db: DbSession,
+    current_user: Annotated[User, Depends(get_current_active_user)],
+) -> AllUsersResponse:
+    if current_user.role.name != "admin":
+        raise ForbidenError(detail="Not authorized to perform this action")
+
     users = crud_user.get_all_users(db)
-    return AllUsersResponse(
-        users=users,
-        total=len(users),
-    )
+
+    return AllUsersResponse(users=users, total=len(users))
