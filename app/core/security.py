b897194de6b43da675bbb3b5a71996bd78c25432
Juan Herrera <juanherreramen13@gmail.com>
2026-01-10 21:56:42 -0500

    feat: #2 add jwt flow
    - Add creation and decoding functions
    - Add custom exceptions

--- app/core/security.py
+++ app/core/security.py
@@ -1,6 +1,36 @@
+from datetime import UTC, datetime, timedelta
+from typing import Any
+
+import jwt
 from passlib.context import CryptContext
 
+from app.config import get_settings
+from app.exceptions import UnautorizedError
+
+ALGORITHM = "HS256"
+DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES = 15
+
+settings = get_settings()
+
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+
+def create_access_token(
+    data: dict,
+    expires_delta: timedelta = timedelta(minutes=DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES),
+) -> str:
+    to_encode = data.copy()
+    expires = datetime.now(UTC) + expires_delta
+    to_encode.update({"exp": expires})
+
+    return jwt.encode(payload=to_encode, key=settings.app.jwt_secret, algorithm=ALGORITHM)
+
+
+def decode_token(token: str) -> dict[str, Any]:
+    try:
+        return jwt.decode(token.strip(), key=settings.app.jwt_secret, algorithms=ALGORITHM)
+    except jwt.InvalidTokenError as err:
+        raise UnautorizedError(detail="Could not validate credentials") from err
 
 
 def hash_password(password: str) -> str:
