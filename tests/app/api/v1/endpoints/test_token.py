2c303210bb02aae4e0b3ceb863fb15c71f0d0745
Juan Herrera <juanherreramen13@gmail.com>
2026-01-26 22:29:25 -0500

    feat: #2 add tests
    - Add test for user endpoint
    - Improve tests for token endpoint

--- tests/app/api/v1/endpoints/test_token.py
+++ tests/app/api/v1/endpoints/test_token.py
@@ -1,12 +1,13 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
+from app.api.v1.schemas.router import TokenResponse
 from tests.factories.user import UserFactory
 
 
 def test_get_access_token(db_client: TestClient) -> None:
     password = "test-password"  # noqa: S105
-    user = UserFactory(password=password)
+    user = UserFactory.create(password=password)
 
     login_request = {
         "email": user.email,
@@ -14,10 +15,9 @@ def test_get_access_token(db_client: TestClient) -> None:
     }
     response = db_client.post("/api/v1/token", json=login_request)
 
-    assert response.status_code is status.HTTP_200_OK
+    assert response.status_code == status.HTTP_200_OK
 
-    response_json = response.json()
-    assert response_json
+    token = TokenResponse.model_validate(response.json())
 
-    assert response_json["token"]
-    assert response_json["type"] == "Bearer"
+    assert token.token
+    assert token.type == "Bearer"
