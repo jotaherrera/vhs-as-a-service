9a597734509d263218ddaebf7c369e11832ee00a
Juan Herrera <juanherreramen13@gmail.com>
2026-01-27 00:00:08 -0500

    feat: #2 update tests
    - Improve some test
    - Add more coverage

--- tests/app/api/v1/endpoints/test_token.py
+++ tests/app/api/v1/endpoints/test_token.py
@@ -1,7 +1,7 @@
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from app.api.v1.schemas.router import TokenResponse
+from app.api.v1.schemas.router import TokenRequest, TokenResponse
 from tests.factories.user import UserFactory
 
 
@@ -9,10 +9,10 @@ def test_get_access_token(db_client: TestClient) -> None:
     password = "test-password"  # noqa: S105
     user = UserFactory.create(password=password)
 
-    login_request = {
-        "email": user.email,
-        "password": password,
-    }
+    login_request = TokenRequest(
+        email=user.email,
+        password=password,
+    ).model_dump()
     response = db_client.post("/api/v1/token", json=login_request)
 
     assert response.status_code == status.HTTP_200_OK
@@ -21,3 +21,14 @@ def test_get_access_token(db_client: TestClient) -> None:
 
     assert token.token
     assert token.type == "Bearer"
+
+
+def test_get_token_unauthorized(db_client: TestClient) -> None:
+    login_request = TokenRequest(
+        email="jhondoe@mail.com",
+        password="test-password",  # noqa: S106
+    ).model_dump()
+    response = db_client.post("/api/v1/token", json=login_request)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json()["detail"] == "Invalid email or password"
