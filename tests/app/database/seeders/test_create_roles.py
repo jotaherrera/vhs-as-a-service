7f7a352d1bc567b6ade7bc605f21dbc6a978abdd
Juan Herrera <juanherreramen13@gmail.com>
2026-01-27 21:07:08 -0500

    feat: #2 add test coverage
    - Add test coverage for the role seeder

--- tests/app/database/seeders/test_create_roles.py
+++ tests/app/database/seeders/test_create_roles.py
@@ -1,7 +1,11 @@
+import logging
+
+import pytest
 from sqlalchemy.orm import Session
 
 from app.database.seeders.create_roles import create_roles
 from app.models import Role
+from tests.factories.role import RoleFactory
 
 
 def test_create_roles(db_session: Session) -> None:
@@ -12,3 +16,15 @@ def test_create_roles(db_session: Session) -> None:
     returned_names = {r.name for r in all_roles}
 
     assert returned_names == {"admin", "user"}
+
+
+def test_create_roles_name_already_exists(
+    db_session: Session,
+    caplog: pytest.LogCaptureFixture,
+) -> None:
+    role = RoleFactory.create(name="user")
+
+    with caplog.at_level(logging.WARNING):
+        create_roles(db_session)
+
+    assert f"Role {role.name} already exists" in caplog.text
