9e855d98f908430bab40fb33d25894fceb4fb14f
Juan Herrera <juanherreramen13@gmail.com>
2026-04-02 19:12:15 -0500

    feat: #111 update models
    - Update movie model with is_active and rental_price
    - Update types in the rental role model
    - Update migration file

--- app/models/movie.py
+++ app/models/movie.py
@@ -1,8 +1,9 @@
 from datetime import date, datetime
 from typing import ClassVar
 
-from sqlalchemy import Date, DateTime, Integer, String, Text, func
+from sqlalchemy import Boolean, Date, DateTime, Integer, String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.sql.sqltypes import Float
 
 from app.models import Base
 
@@ -20,7 +21,7 @@ class Movie(Base):
     critic_rating: Mapped[int] = mapped_column(Integer, nullable=False)
     age_rating: Mapped[str] = mapped_column(String(10), nullable=False)
     release_date: Mapped[date] = mapped_column(Date, nullable=False)
-
+    rental_price: Mapped[float] = mapped_column(Float, nullable=False)
     copies_available: Mapped[int] = mapped_column(Integer, nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(
@@ -29,6 +30,8 @@ class Movie(Base):
         nullable=False,
     )
 
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
     rentals = relationship("Rental", back_populates="movie")
 
     def __repr__(self) -> str:
@@ -36,5 +39,6 @@ class Movie(Base):
             f"Movie(id={self.id}, title={self.title!r}, genre={self.genre!r}, "
             f"director={self.director!r}, critic_rating={self.critic_rating}, "
             f"age_rating={self.age_rating!r}, release_date={self.release_date}, "
-            f"copies_available={self.copies_available})"
+            f"copies_available={self.copies_available}), rental_price={self.rental_price}, "
+            f"is_active={self.is_active}"
         )
