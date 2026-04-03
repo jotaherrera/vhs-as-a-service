c86032a9603f7f485b39c3c2bf5df73840d1ec20
Juan Herrera <juanherreramen13@gmail.com>
2026-04-02 20:18:07 -0500

    feat: #111 update movie factory
    - Add the new fields to the movie factory

--- tests/factories/movie.py
+++ tests/factories/movie.py
@@ -19,3 +19,5 @@ class MovieFactory(SQLAlchemyModelFactory):
     age_rating = "PG-13"
     release_date = date(1999, 3, 31)
     copies_available = Faker("random_int", min=0, max=50)
+    rental_price = Faker("pyfloat", min_value=0.99, max_value=5.99, right_digits=2)
+    is_active = True
