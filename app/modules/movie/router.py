465f63901b49a5f0cbad46e102534c80fb69d7a3
Juan Herrera <juanherreramen13@gmail.com>
2026-05-03 17:14:56 -0500

    feat: #112 add get movie endpoint
    - Add get movie endpoint
    - Add get by id to movie service

--- app/modules/movie/router.py
+++ app/modules/movie/router.py
@@ -13,3 +13,12 @@ async def list_movies(
     current_user: CurrentUserDep,
 ) -> list[MovieResponsePublic] | list[MovieResponsePrivate]:
     return service.list_movies(current_user)
+
+
+@router.get("/{movie_id}")
+async def get_movie(
+    service: MovieServiceDep,
+    current_user: CurrentUserDep,
+    movie_id: int,
+) -> MovieResponsePublic | MovieResponsePrivate:
+    return service.get_by_id(current_user, movie_id)
