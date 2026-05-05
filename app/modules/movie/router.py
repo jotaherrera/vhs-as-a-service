85b1b1b4708c89ffb36a268e4ac6b15647031f28
Juan Herrera <juanherreramen13@gmail.com>
2026-05-04 22:37:19 -0500

    feat: #112 add new endpoint
    - Add the add movie endpoint

--- app/modules/movie/router.py
+++ app/modules/movie/router.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from app.modules.auth.dependencies import CurrentActiveUserDep
-from app.modules.movie.schemas import MovieResponsePrivate, MovieResponsePublic
+from app.modules.movie.schemas import (
+    MovieCreate,
+    MovieResponsePrivate,
+    MovieResponsePublic,
+)
 from app.modules.movie.service import MovieServiceDep
 
 router = APIRouter(prefix="/movies", tags=["movies"])
@@ -22,3 +26,12 @@ async def get_movie(
     movie_id: int,
 ) -> MovieResponsePublic | MovieResponsePrivate:
     return service.get_by_id(current_user, movie_id)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def add_movie(
+    service: MovieServiceDep,
+    current_user: CurrentActiveUserDep,
+    request: MovieCreate,
+) -> MovieResponsePrivate:
+    return service.register(current_user, request)
